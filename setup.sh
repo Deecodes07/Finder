8549d2d5a09a7f83e1e3ed1e2423db4c321c915b
Deecodes07 <122016491+Deecodes07@users.noreply.github.com>
2023-02-13 11:07:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+read -p "Please enter your name:" name
+
+#Installation of waybackurls
 sudo apt install golang
 go install
 github.com/tomnomnom/waybackurls@latest
@@ -8,16 +11,22 @@ sudo mv waybackurls /usr/bin
 
 clear
 
+#Installation of figlet
 sudo apt install figlet -y
 
 clear
 
+#installation of gobuster
 sudo apt install gobuster -y
 
 clear
 
+#installation of dirb
 sudo apt install dirb -y
 
 clear
 
-figlet Done .!..
+#installation of cowsay
+sudo apt install cowsay -y
+
+figlet $name Done .!..
